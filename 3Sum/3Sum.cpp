4475543545f4a0d0d6fb313abfe2e3531975ee3f
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2021-12-05 14:37:18 +0300

    TL

--- 3Sum/3Sum.cpp
+++ 3Sum/3Sum.cpp
@@ -1,68 +1,98 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <unordered_set>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
+
+struct VectorHash {
+    size_t operator()(const std::vector<int>& v) const {
+        std::hash<int> hasher;
+        size_t seed = 0;
+        for (int i : v) {
+            seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        }
+        return seed;
+    }
+};
+
+
 class Solution {
 public:
     
     struct coord {
-        coord(int _i, int _j) : i(_i), j (_j) {};
+        coord(int& _i, int& _j) : i(_i), j (_j) {};
         int i;
         int j;
     };
-    
 
-    bool duplicate(int i, int j, int k) {
-
-        return i == j or j == k or i == k;
-
-    }
 
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        map<int, int> num;
-
+        map<int, unordered_set<int>> indexes;
         int n = nums.size();
 
-        for (int i = 0;i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
 
-            num[nums[i]] = i;
+            if (indexes.find(nums[i]) == indexes.end()) {
+                indexes[nums[i]] = unordered_set<int>();
+            }
+            indexes[nums[i]].insert(i);
 
         }
 
-        vector<vector<int>> res;
-        set<vector<int>> set_res;
+        map<int, vector<coord>> sum;
         for (int i = 0; i < n; ++i) {
 
             for (int j = i + 1; j < n; ++j) {
 
-                if (num.find(-nums[i] - nums[j]) != num.end()) {
+                int s = nums[i] + nums[j];
+                if (sum.find(s) == sum.end()) {
 
-                    int k = num[-nums[i] - nums[j]];
-                    std::vector<int> v = { nums[i], nums[j], nums[k] };
-                    int min = v[std::distance(std::min_element(v.begin(), v.end()), v.begin())];
-                    int max = v[std::distance(std::max_element(v.begin(), v.end()), v.begin())];
-                    int avg = nums[i] + nums[j] + nums[k] - min - max;
-                    v = {min, avg, max};
-                    if ((not duplicate(i, j, k)) and (set_res.find(v) == set_res.end())) {
+                    sum[s] = {};
 
-                        set_res.insert(v);
-                        res.push_back(v);
+                }
+                sum[s].push_back(coord(i, j));
+
+            }
+
+        }
+
+        vector<vector<int>> res;
+        unordered_set<vector<int>, VectorHash> set_res;
+        for (auto& pairs: sum) {
+
+            if (indexes.find(-pairs.first) != indexes.end()) {
+
+                for (auto& p: pairs.second) {
+
+                    int num_in = (indexes[-pairs.first].find(p.i) != indexes[-pairs.first].end()) + (indexes[-pairs.first].find(p.j) != indexes[-pairs.first].end());
+                    if (indexes[-pairs.first].size() > num_in) {
+
+                        std::vector<int> v = { nums[p.i], nums[p.j], -pairs.first };
+                        std::sort(v.begin(), v.end());
+
+                        if (set_res.find(v) == set_res.end()) {
+
+                            set_res.insert(v);
+                            res.push_back(v);
+
+                        }
 
                     }
 
-                }
 
+                }
 
             }
 
         }
 
         return res;
+
 
     }
 };
